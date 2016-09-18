97b79861bc2da4db0948cf27da501972ee7e2953
Ferenc Kurucz <ferenc.kurucz@outlook.com>
2016-09-18 20:12:23 +0200

    update/insert/delete t_partner

--- queries.js
+++ queries.js
@@ -48,10 +48,97 @@ function getSingleCustomer(req, res, next) {
     });
 }
 
+// --- PARTNER ---
+function getAllPartners(req, res, next) {
+  db.any('select * from public.t_partner')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL partners'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getSinglePartner(req, res, next) {
+  var partner_id = parseInt(req.params.id);
+  db.one('select * from public.t_partner where id = $1', partner_id)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE partner, id:' + partner_id
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function createPartner(req, res, next) {
+  //req.body.age = parseInt(req.body.age);
+  db.none('insert into public.t_partner(abbreviation, name, remark)' +
+      'values(${abbreviation}, ${name}, ${remark})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one Partner'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function updatePartner(req, res, next) {
+  var partner_id = parseInt(req.params.id);
+  db.none('update public.t_partner set abbreviation=$1, name=$2, remark=$3 where id=$4',
+      [req.body.abbreviation, req.body.name, req.body.remark,
+        partner_id])
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Updated partner id:' + partner_id
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function removePartner(req, res, next) {
+  var partner_id = parseInt(req.params.id);
+  db.result('delete from public.t_partner where id = $1', partner_id)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} partner`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+
 module.exports = {
   getAllCustomers: getAllCustomers,
   getSingleCustomer: getSingleCustomer,
-  /*createCustomer: createCustomer,
-  updateCustomer: updateCustomer,
-  removeCustomer: removeCustomer*/
+  getAllPartners: getAllPartners,
+  getSinglePartner: getSinglePartner,
+  createPartner: createPartner,
+  updatePartner: updatePartner,
+  removePartner: removePartner
 };
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ var db = require('../queries');
 
 router.get('/api/customers', db.getAllCustomers);
 router.get('/api/customers/:id', db.getSingleCustomer);
-/*router.post('/api/customers', db.createCustomer);
-router.put('/api/customers/:id', db.updateCustomer);
-router.delete('/api/customers/:id', db.removeCustomer);*/
+router.get('/api/partners', db.getAllPartners);
+router.get('/api/partners/:id', db.getSinglePartner);
+router.post('/api/partners', db.createPartner);
+router.put('/api/partners/:id', db.updatePartner);
+router.delete('/api/partners/:id', db.removePartner);
 
 module.exports = router;
